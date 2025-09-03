379a92ec00643fc064bd69dc07c3a73bdba1d4f7
Andrews Arokiam <andrews.arokiam@ideas2it.com>
2025-09-03 10:55:24 +0530

    Add unit test for email extract

--- src-tauri/src/pgplib.rs
+++ src-tauri/src/pgplib.rs
@@ -109,38 +109,7 @@ pub fn decrypt_message(app_handle: tauri::AppHandle, encrypted_text: &str, passp
         .path()
         .app_data_dir()
         .unwrap();
-    // let private_key_path = app_data_path.join("private_key.asc");
-    // // add print statement for passphrase
-    // println!("Decrypting message with passphrase: {}", passphrase);
-    // // Read the private key from the specified file
-    // let private_key_str = std::fs::read_to_string(private_key_path)?;
-    // println!("Private key string: {}", private_key_str);
-    // let private_key = SignedSecretKey::from_string(&private_key_str)?.0;
-    // println!("Private key successfully loaded.");
 
-    // // Decrypt the message with the private key
-    // let message = Message::from_armor_single(encrypted_text.as_bytes())
-    //     .map_err(|e| LibError::PgpError(pgp::errors::Error::Message(format!("Failed to parse encrypted message: {}", e))))?
-    //     .0;
-
-    // println!("Message successfully parsed.");
-
-    // let decrypted_message: (Message, Vec<pgp::types::KeyId>) = message
-    //     .decrypt(|| passphrase.to_string(), &[&private_key])
-    //     .map_err(|e| LibError::PgpError(pgp::errors::Error::Message(format!("Failed to decrypt message (check passphrase and key): {}", e))))?;
-
-    // println!("Message successfully decrypted.");
-    // let decrypted_message = decrypted_message.0.decompress()
-    //     .map_err(|e| LibError::PgpError(pgp::errors::Error::Message(format!("Failed to decompress message: {}", e))))?;
-    
-    // println!("Message successfully decompressed.");
-    // let decrypted_content = decrypted_message.get_content()
-    //     .map_err(|e| LibError::PgpError(pgp::errors::Error::Message(format!("Failed to get message content: {}", e))))?
-    //     .ok_or_else(|| LibError::PgpError(pgp::errors::Error::Message("Message content is empty".to_string())))?;
-
-    // println!("Message content successfully retrieved.");
-    // String::from_utf8(decrypted_content)
-    //     .map_err(|e| LibError::PgpError(pgp::errors::Error::Message(format!("Failed to convert message to UTF-8: {}", e))))
     let private_key_path = app_data_path.join("private_key.asc");
     let private_key_str = std::fs::read_to_string(private_key_path)?;
     let private_key = SignedSecretKey::from_string(&private_key_str)?.0;
@@ -283,9 +252,8 @@ mod tests {
         let message = Message::from_armor_single(encrypted_message.as_bytes()).unwrap().0;
         println!("Encrypted text: {}", encrypted_message);
         println!("Decrypting message with passphrase: {}", passphrase);
-        let passphrase1 = 's';
         let decrypted_message: (Message, Vec<pgp::types::KeyId>) = message
-            .decrypt(|| passphrase1.to_string(), &[&private_key])
+            .decrypt(|| passphrase.to_string(), &[&private_key])
             .unwrap();
 
         let decrypted_message = decrypted_message.0.decompress().unwrap();
@@ -301,6 +269,36 @@ mod tests {
         assert_ne!(decrypted_text, encrypted_message);
         
         println!("✓ Decrypt message test passed!");
+    }
+
+    #[test]
+    fn test_read_email_from_public_key() {
+        let temp_dir = TempDir::new().unwrap();
+        let app_data_path = temp_dir.path().join("app_data");
+        println!("Test App Data Path: {:?}", app_data_path);
+        fs::create_dir_all(&app_data_path).unwrap();
+        
+        let user_id = "test@example.com";
+        let passphrase = "test_passphrase";
+     
+        // Create test keys
+        create_test_keys(&app_data_path, user_id, passphrase);
+        // Read the public key from the specified file
+        let public_key_path = app_data_path.join("public_key.asc");
+        let public_key_str = std::fs::read_to_string(public_key_path).unwrap();
+        let public_key = SignedPublicKey::from_string(&public_key_str).unwrap().0;
+        let user_ids: Vec<String> = public_key
+            .details
+            .users
+            .iter()
+            .map(|uid| uid.id.clone().id().to_string())
+            .collect();
+
+        let found_user_id = user_ids[0].clone();
+        println!("User ID found in public key: {}", found_user_id);   
+
+        assert!(user_ids.contains(&user_id.to_string()));
+        println!("✓ Read email from public key test passed!");
     }
 }
 
